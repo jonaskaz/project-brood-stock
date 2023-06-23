5f41a26acc4b0fdaebb9fd768b910c07bc73c48c
Matthew Phillips <matthew.a.phillips@wmich.edu>
2023-06-23 19:00:15 -0400

    Reduced down to one function for dimming.

--- src/main.cpp
+++ src/main.cpp
@@ -7,23 +7,22 @@ const int maxBrightness = 255.0;
 int brightness;
 unsigned long time;
 
-void dimUp(double);
-void dimDown(unsigned long);
+void dimmerControl(double, bool);
 
 void setup() {
   Serial.begin(9600);
   pinMode(led, OUTPUT);
-  time = 60000; //10 minutes
+  time = 60000; //1 minute
   brightness = 0;
 }
 
 void loop() {
-  dimUp(time);
-  dimDown(time*2);
+  dimmerControl(time, true);
+  dimmerControl(time * 2, false);
 }
 //ramps up the brightness of the light
 //also written to handle millis() overflow, I hope
-void dimUp(double rampTime) {
+void dimmerControl(double rampTime, bool increase) {
 
   unsigned long currTime = millis();
   unsigned long prevTime = currTime;
@@ -39,47 +38,24 @@ void dimUp(double rampTime) {
     }else{//add elapsed time 
       elapsedTime += (currTime - prevTime);
     }
-    double ratio = elapsedTime/rampTime;
-    brightness = round(maxBrightness * ratio);
-    
-    //write to led, may need if statement to switch from DAC to converter pin
-    analogWrite(led, brightness);
-    prevTime = currTime;
-
-    Serial.print("Ramp up time: ");
-    Serial.print(elapsedTime);
-    Serial.print(" Brightness: ");
-    Serial.println(brightness);
-  }
-}
-
-void dimDown(unsigned long dimTime){
-
-  unsigned long currTime = millis();
-  unsigned long prevTime = currTime;
-  double elapsedTime = 0;
-
-  while(elapsedTime < dimTime) {
-    currTime = millis();
-    //if we have overflowed add from prev time to upper bound,
-    //then add from 0 to current time
-    if(currTime < prevTime){
-      elapsedTime += (maxMillis - prevTime);
-      elapsedTime += currTime;
-    }else{//add elapsed time 
-      elapsedTime += (currTime - prevTime);
+    double ratio;
+    //dim up or down based on what is needed
+    if(increase) {
+      ratio = elapsedTime/rampTime;
+      Serial.print("Ramp up time: ");
+    }else{
+      ratio = 1 - (elapsedTime/rampTime);
+      Serial.print("Dim Down: ");
     }
-    double ratio = 1 - (elapsedTime/dimTime);
     brightness = round(maxBrightness * ratio);
     
     //write to led, may need if statement to switch from DAC to converter pin
     analogWrite(led, brightness);
     prevTime = currTime;
 
-    Serial.print("Dim down time: ");
+    
     Serial.print(elapsedTime);
     Serial.print(" Brightness: ");
     Serial.println(brightness);
   }
-
 }
