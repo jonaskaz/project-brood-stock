9906eecfc8125491c840b0f4dfbf740f45084a30
Matthew Phillips <matthew.a.phillips@wmich.edu>
2023-06-23 15:59:59 -0400

    Got the base of dimUp and dimDown working, overflow accounted for. Need to set pins to change between DAC and converter, if needed.

--- src/main.cpp
+++ src/main.cpp
@@ -1,54 +1,85 @@
 #include <Arduino.h>
+#include <Adafruit_BusIO_Register.h>
 
 const int led = 5;
-bool increase = true;
-int brightness = 0;
+const unsigned long maxMillis = 4294967295;
+const int maxBrightness = 255.0;
+int brightness;
+unsigned long time;
 
-void setup() { 
+void dimUp(double);
+void dimDown(unsigned long);
 
+void setup() {
+  Serial.begin(9600);
   pinMode(led, OUTPUT);
-
+  time = 60000; //10 minutes
+  brightness = 0;
 }
 
 void loop() {
+  dimUp(time);
+  dimDown(time*2);
+}
+//ramps up the brightness of the light
+//also written to handle millis() overflow, I hope
+void dimUp(double rampTime) {
 
-  analogWrite(led, brightness);
-  dimUp();
-  delay(50);
+  unsigned long currTime = millis();
+  unsigned long prevTime = currTime;
+  double elapsedTime = 0;
 
+  while(elapsedTime < rampTime) {
+    currTime = millis();
+    //if we have overflowed add from prev time to upper bound,
+    //then add from 0 to current time
+    if(currTime < prevTime){
+      elapsedTime += (maxMillis - prevTime);
+      elapsedTime += currTime;
+    }else{//add elapsed time 
+      elapsedTime += (currTime - prevTime);
+    }
+    double ratio = elapsedTime/rampTime;
+    brightness = round(maxBrightness * ratio);
+    
+    //write to led, may need if statement to switch from DAC to converter pin
+    analogWrite(led, brightness);
+    prevTime = currTime;
+
+    Serial.print("Ramp up time: ");
+    Serial.print(elapsedTime);
+    Serial.print(" Brightness: ");
+    Serial.println(brightness);
+  }
 }
 
-void dimUp(){
+void dimDown(unsigned long dimTime){
 
-  analogWrite(led, brightness);
-  if (increase) {
-    brightness += 1;
-    if(brightness >= 150){
-    increase = false;
+  unsigned long currTime = millis();
+  unsigned long prevTime = currTime;
+  double elapsedTime = 0;
+
+  while(elapsedTime < dimTime) {
+    currTime = millis();
+    //if we have overflowed add from prev time to upper bound,
+    //then add from 0 to current time
+    if(currTime < prevTime){
+      elapsedTime += (maxMillis - prevTime);
+      elapsedTime += currTime;
+    }else{//add elapsed time 
+      elapsedTime += (currTime - prevTime);
     }
-  }else{
-    brightness -= 1;
-    if(brightness <= 0) {
-      increase = true;
-    }
+    double ratio = 1 - (elapsedTime/dimTime);
+    brightness = round(maxBrightness * ratio);
+    
+    //write to led, may need if statement to switch from DAC to converter pin
+    analogWrite(led, brightness);
+    prevTime = currTime;
+
+    Serial.print("Dim down time: ");
+    Serial.print(elapsedTime);
+    Serial.print(" Brightness: ");
+    Serial.println(brightness);
   }
-
-}
-
-void dimDown(){
-
-  analogWrite(led, brightness);
-  if (increase) {
-    brightness += 1;
-    if(brightness >= 150){
-    increase = false;
-    }
-  }else{
-    brightness -= 1;
-    if(brightness <= 0) {
-      increase = true;
-    }
-  }
-  delay(50);
 
 }
