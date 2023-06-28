44ef72ba5d5e00abcf689f00a76316ca0a8362fb
jonaskaz <jonas.kazlauskas@me.com>
2023-06-28 11:26:42 -0400

    Move pio to v1 folder and add plotting

---
+++ v1/src/main.cpp
@@ -0,0 +1,107 @@
+#include <Adafruit_MCP4728.h>
+#include <Arduino.h>
+#include <Wire.h>
+#include <ezButton.h>
+
+#define CLOCKPIN 7
+
+ezButton clock(CLOCKPIN);
+Adafruit_MCP4728 mcp;
+
+const MCP4728_channel_t DACCHANNEL = MCP4728_CHANNEL_A;
+const int MAXBRIGHTNESS = 4095;
+const int MINBRIGHTNESS = 500;
+const int INCREMENT = 10;
+const int LOOPDELAY = 30;
+const long RAMPTIME = 600000; // 10 minutes
+
+int brightness = MINBRIGHTNESS;
+int startBrightness = brightness;
+unsigned long elapsedTime = 0;
+unsigned long startTime;
+
+bool isClockOn();
+void setupMCP();
+void setDacValue(int value);
+void updateSunriseBrightness();
+void updateSunsetBrightness();
+void updateElapsedTime();
+
+void setup() {
+  Serial.begin(115200);
+  clock.setDebounceTime(200);
+  setupMCP();
+  startTime = millis();
+}
+
+void loop() {
+  clock.loop();
+  if (clock.isPressed() || clock.isReleased()) {
+    startTime = millis();
+    elapsedTime = 0;
+    startBrightness = brightness;
+  }
+  int prevBrightness = brightness;
+  if (isClockOn()) {
+    updateElapsedTime();
+    updateSunriseBrightness();
+  } else {
+    updateElapsedTime();
+    updateSunsetBrightness();
+  }
+  if (prevBrightness != brightness) {
+    setDacValue(brightness);
+  }
+  Serial.print("Brightness: ");
+  Serial.println(brightness);
+  Serial.print("Elapsed: ");
+  Serial.println(elapsedTime);
+  Serial.print("Timestamp: ");
+  Serial.println(millis());
+}
+
+bool isClockOn() {
+  int state = clock.getState();
+  return (state == LOW);
+}
+
+void setupMCP() {
+  while (!Serial)
+    delay(10);
+
+  if (!mcp.begin()) {
+    Serial.println("Failed to find MCP4728 chip");
+    while (1) {
+      delay(10);
+    }
+  }
+}
+
+void setDacValue(int value) {
+  value = constrain(value, MINBRIGHTNESS, MAXBRIGHTNESS);
+  mcp.setChannelValue(DACCHANNEL, value, MCP4728_VREF_INTERNAL,
+                      MCP4728_GAIN_2X);
+}
+
+void updateElapsedTime() {
+  unsigned long currTime = millis();
+  elapsedTime = (currTime - startTime);
+}
+
+void updateSunriseBrightness() {
+  unsigned long brightnessBuffer =
+      map(startBrightness, MINBRIGHTNESS, MAXBRIGHTNESS, 0, RAMPTIME);
+  Serial.print("SunriseBuffer: ");
+  Serial.println(brightnessBuffer);
+  brightness = min(MAXBRIGHTNESS, map(elapsedTime + brightnessBuffer, 0,
+                                      RAMPTIME, MINBRIGHTNESS, MAXBRIGHTNESS));
+}
+
+void updateSunsetBrightness() {
+  unsigned long brightnessBuffer =
+      map(startBrightness, MAXBRIGHTNESS, MINBRIGHTNESS, 0, RAMPTIME);
+  Serial.print("SunsetBuffer: ");
+  Serial.println(brightnessBuffer);
+  brightness = max(MINBRIGHTNESS, map(elapsedTime + brightnessBuffer, RAMPTIME,
+                                      0, MINBRIGHTNESS, MAXBRIGHTNESS));
+}
