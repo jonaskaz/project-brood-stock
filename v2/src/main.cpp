9e0e63c25b6387960cd2cf33171063e7dfa395aa
jonaskaz <jonas.kazlauskas@me.com>
2023-07-06 17:01:55 -0400

    Add metro mini and instructions for flashing

--- v2/src/main.cpp
+++ v2/src/main.cpp
@@ -25,10 +25,7 @@ void loop() {
   // (note: line 1 is the second row, since counting begins with 0):
   lcd.setCursor(0, 1);
   // print the number of seconds since reset:
-  lcd.print(millis()/1000);
-
+  lcd.print(millis());
   lcd.setBacklight(HIGH);
-  delay(500);
-  lcd.setBacklight(LOW);
   delay(500);
 }
